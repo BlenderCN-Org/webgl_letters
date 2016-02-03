2f923e140e3b7a53b680b05b7399054945870733
Angel Angelov <hextwoa@gmail.com>
2016-02-03 13:09:39 +0200

    calculate letter width

--- letters.py
+++ letters.py
@@ -1,11 +1,13 @@
 import bpy
 
+def object_width(object):
+    bpy.context.scene.update()
+    return object.dimensions.x
+
 def make_letter(letter, **options):
     bpy.ops.object.text_add()
     text_object = bpy.context.object
     text = text_object.data
-    
-    text.body = letter
     
     text.resolution_u = options.get('resolution', 5)
     text.resolution_v = options.get('resolution', 5)
@@ -21,9 +23,19 @@ def make_letter(letter, **options):
     
     text.font = options.get('font', bpy.data.fonts[0])
     
+    padding = options.get('width_padding', 'gs')
+    
+    text.body = padding + padding
+    padding_width = object_width(text_object)
+    
+    text.body = padding + letter + padding
+    width = object_width(text_object) - padding_width
+    
+    text.body = letter
+    
     bpy.ops.object.convert(target='MESH')
     
-    return text_object
+    return (text_object, width)
 
 
-make_letter('gs', resolution=10)
+print(make_letter('Q', resolution=10))
