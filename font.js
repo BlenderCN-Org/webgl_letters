3f60affed4ca7148a55bfe096ea1145ff74245c5
Angel Angelov <hextwoa@gmail.com>
2016-02-09 00:18:15 +0200

    implement some webgl code

---
+++ font.js
@@ -0,0 +1,152 @@
+CanonicalFont = function(letters) {
+    var flatten = function(arrays) {
+        return [].concat.apply([], arrays)
+    }
+
+    var get_normal = function(triangle) {
+        return vectorProduct(
+                vectorPoints(triangle[1], triangle[0]),
+                vectorPoints(triangle[2], triangle[0])
+        )
+    }
+
+    this.letters = {};
+    
+    var data = [];
+    var data_index = 0;
+
+    for (var symbol in letters) {
+        this.letters[symbol] = {
+            "kerning_width": letters[symbol].kerning_width,
+            "start": data_index * 3,
+            "vertex_count": letters[symbol].mesh.length * 3
+        }
+        var triangles = letters[symbol].mesh;
+        for (var i = 0; i < triangles.length; i++) {
+            normal = get_normal(triangles[i]);
+            data.push(
+                triangles[i][0][0], triangles[i][0][1], triangles[i][0][2],
+                normal[0]     , normal[1]     , normal[2],
+
+                triangles[i][1][0], triangles[i][1][1], triangles[i][1][2],
+                normal[0]     , normal[1]     , normal[2],
+
+                triangles[i][2][0], triangles[i][2][1], triangles[i][2][2],
+                normal[0]     , normal[1]     , normal[2]
+            );
+            data_index++;
+        }
+    }
+
+    var buf = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buf);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+    this.buf = buf;
+}
+
+CanonicalFont.prototype.drawLetter = function(symbol) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.buf);
+
+    gl.enableVertexAttribArray(aXYZ);
+    gl.vertexAttribPointer(aXYZ, 3, gl.FLOAT, false, 6*FLOATS, 0*FLOATS);
+
+    gl.enableVertexAttribArray(aNormal);
+    gl.vertexAttribPointer(aNormal, 3, gl.FLOAT, false, 6*FLOATS, 3*FLOATS);
+
+    var letter = this.letters[symbol];
+    gl.drawArrays(gl.TRIANGLES, letter.start, letter.vertex_count);
+}
+
+CanonicalFont.prototype.letterKerning = function(symbol) {
+    return this.letters[symbol].kerning_width;
+}
+
+fonts = {}
+font_data = {}
+
+registerFont = function(name, data) {
+    font_data[name] = data;
+}
+
+getFont = function(name) {
+    font = fonts[name];
+    if (!font) {
+        font = new CanonicalFont(font_data[name]);
+        delete font_data[name];
+        fonts[name] = font;
+    }
+    return font;
+}
+
+Text = function(font_name, body, center, size) {
+    this.font = getFont(font_name);
+
+	this.center = center || [0, 0, 0];
+	this.size = size || 1;
+	this.color = [1,0.75,0];
+	this.offset = undefined;
+	this.rot = undefined;
+
+	this.lineSpacing = 0.8;
+	this.letterSpacing = 0;
+	this.kerning = 1;
+	this.centered = false;
+
+    this.depth = 1;
+
+    this.set(body)
+}
+
+Text.prototype.set = function(body) {
+    this.lines = body.split('\n');
+    this.line_widths = [];
+    this.width = 0;
+    this.height = 0;
+
+    var line_width = 0;
+    for (var i = 0; i < this.lines.length; i++) {
+        line_width = 0;
+        for (var j = 0; j < this.lines[i].length; j++) {
+            letter = this.lines[i][j];
+            line_width += this.font.letterKerning(letter) * this.kerning +
+                          this.letterSpacing;
+        }
+        if (this.width < line_width) {
+            this.width = line_width;
+        }
+        this.line_widths.push(line_width);
+        this.height += this.lineSpacing;
+    }
+}
+
+Text.prototype.draw = function() {
+	pushMatrix();
+	gl.vertexAttrib3fv(aColor, this.color); // подаваме цвета
+	translate(this.center);
+	if (this.rot)
+	{
+		if (this.rot[0]) zRotate(this.rot[0]);
+		if (this.rot[1]) xRotate(this.rot[1]);
+		if (this.rot[2]) zRotate(this.rot[2]);
+	}
+	scale([this.size,this.size,this.size * this.depth]);
+	if (this.offset) translate(this.offset);
+
+    for (var i = 0; i < this.lines.length; i++) {
+        pushMatrix();
+        if (this.centered) {
+            translate([(this.width - this.line_widths[i]) / 2, 0, 0]);
+        }
+        for (var j = 0; j < this.lines[i].length; j++) {
+            letter = this.lines[i][j];
+            useMatrix();
+            this.font.drawLetter(letter);
+            translate([this.font.letterKerning(letter) * this.kerning
+                    + this.letterSpacing, 0, 0]);
+        }
+        popMatrix();
+        translate([0, - this.lineSpacing, 0]);
+    }
+
+	popMatrix();
+}
--- letters.py
+++ letters.py
@@ -1,10 +1,19 @@
 import bpy
+import json
+import bmesh
 
 def object_width(object):
     bpy.context.scene.update()
     return object.dimensions.x
 
-def make_letter(letter, **options):
+def triangulate(mesh):
+    bm = bmesh.new()
+    bm.from_mesh(mesh)
+    bmesh.ops.triangulate(bm, faces=bm.faces)
+    bm.to_mesh(mesh)
+    bm.free()
+
+def build_letter_object(letter, **options):
     bpy.ops.object.text_add()
     text_object = bpy.context.object
     text = text_object.data
@@ -34,8 +43,58 @@ def make_letter(letter, **options):
     text.body = letter
     
     bpy.ops.object.convert(target='MESH')
+    triangulate(text_object.data)
     
     return (text_object, width)
 
+def make_vertex(mesh, face, index):
+    vertex = mesh.vertices[mesh.loops[index].vertex_index]
+    uv_layer = mesh.uv_layers.active
+    data = {
+        'point': list(vertex.co),
+    }
+    if face.use_smooth:
+        data['normal'] = list(vertex.normal)
+    if uv_layer:
+        data['uv'] = list(uv_layer.data[index].uv)
+    return data
 
-print(make_letter('Q', resolution=10))
+def make_simple_vertex(mesh, face, index):
+    vertex = mesh.vertices[mesh.loops[index].vertex_index]
+
+    return list(vertex.co)
+
+def make_face(mesh, face):
+    face_vertices = face.loop_indices
+    if (len(face_vertices) != 3):
+        print("Warning: face is not a triangle")
+    return [make_simple_vertex(mesh, face, index) for index in face_vertices]
+
+
+def make_mesh(mesh):
+    return [make_face(mesh, face) for face in mesh.polygons]
+
+def make_letter(letter, **options):
+    letter_object, width = build_letter_object(letter, **options)
+    mesh = make_mesh(letter_object.data)
+    
+    bpy.ops.object.delete()
+    
+    letter_data = {
+        'kerning_width': width,
+        'mesh': mesh
+    }
+    return (letter, letter_data)
+
+def make_font(letters, **options):
+    return dict([make_letter(letter, **options) for letter in letters])
+
+def json_to_file(data, name, file):
+    with open(file, 'w') as f:
+        f.write("registerFont('" + name + "',")
+        json.dump(data, f, separators=(',', ':'))
+        f.write(");\n")
+
+    
+
+json_to_file(make_font('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456890.?', resolution=3, extrude=0.2), 'uglyFont', '/tmp/testfont.js')
